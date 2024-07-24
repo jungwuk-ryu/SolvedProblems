bc2ea90013460b204f2b3c69b7c46a5e85f3ee1d
Jungwuk Ryu <vojougae35@gmail.com>
2024-07-24 16:47:56 +0900

    [Bronze V] Title: ??!, Time: 224 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);
+        
+        a = sc.next();
+        System.out.println(a + "??!");
+    }
+}
