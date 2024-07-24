7ac4a1b181832975c100ded4805934d8f550b2d8
Jungwuk Ryu <vojougae35@gmail.com>
2024-07-24 16:45:04 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long count = Integer.parseInt(br.readLine());
+
+        for (long i = 1; i <= count; i++) {
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            int a, b;
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            bw.write("Case #");
+            bw.write(Long.toString(i));
+            bw.write(": ");
+            bw.write(Integer.toString(a));
+            bw.write(" + ");
+            bw.write(Integer.toString(b));
+            bw.write(" = ");
+            bw.write(Integer.toString(a + b));
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
