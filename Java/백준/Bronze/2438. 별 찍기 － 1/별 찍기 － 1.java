8ff668f2a7fbecb355a3e8dc920188609a22a60f
Jungwuk Ryu <vojougae35@gmail.com>
2024-07-24 16:44:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 136 ms, Memory: 14180 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long count = Integer.parseInt(br.readLine());
+
+        for (long i = 0; i < count; i++) {
+            for (long l = 0; l <= i; l++) {
+                bw.write('*');
+            }
+            bw.write('\n');
+        }
+
+        bw.flush();
+    }
+}
